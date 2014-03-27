16eba28b170efe4b67ba515aca0bac59aa3ece7b
Jaap Vermaas <jaap@tuxic.nl>
2014-03-27 13:18:21 +0000

    fixed downloads

--- www/download.php
+++ www/download.php
@@ -3,6 +3,6 @@
   if(substr($_GET["file"], -3) == "jpg") header("Content-Type: image/jpeg");
   else header("Content-Type: video/mp4");
   header("Content-Disposition: attachment; filename=\"" . $_GET["file"] . "\"");
-  readfile("../media/" . $_GET["file"]);
+  readfile("media/" . $_GET["file"]);
 
 ?>
